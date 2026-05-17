2c7c3c588e56e17cb312559f0981ddd441c7897c
Oyinade247 <adedokunoyinade@gmail.com>
2026-05-17 13:18:14 +0100

    fixed ci/cd pipeline and docker

--- src/main/java/com/Loan/data/models/Loan.java
+++ src/main/java/com/Loan/data/models/Loan.java
@@ -4,6 +4,7 @@ package com.Loan.data.models;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
 import lombok.Generated;
 import lombok.Getter;
 import lombok.Setter;
@@ -11,6 +12,7 @@ import lombok.Setter;
 import java.math.BigDecimal;
 
 @Entity
+@Table(name = "loan")
 @Getter
 @Setter
 public class Loan {
