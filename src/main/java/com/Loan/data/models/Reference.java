31a092acf481a555c08ab11d2684a557af199c68
Oyinade247 <adedokunoyinade@gmail.com>
2026-05-17 13:23:06 +0100

    fixed ci/cd pipeline and docker

--- src/main/java/com/Loan/data/models/Reference.java
+++ src/main/java/com/Loan/data/models/Reference.java
@@ -3,8 +3,10 @@ package com.Loan.data.models;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "reference")
 public class Reference {
     @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
