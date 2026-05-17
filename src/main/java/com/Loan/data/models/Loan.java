5639250ca70068eed673e7ff188f371444b755eb
Oyinade247 <adedokunoyinade@gmail.com>
2026-05-17 13:22:18 +0100

    fixed ci/cd pipeline and docker

--- src/main/java/com/Loan/data/models/Loan.java
+++ src/main/java/com/Loan/data/models/Loan.java
@@ -5,7 +5,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Table;
-import lombok.Generated;
 import lombok.Getter;
 import lombok.Setter;
 
